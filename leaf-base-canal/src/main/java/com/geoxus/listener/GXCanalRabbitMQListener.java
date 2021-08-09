527a9acb0a0de10070a0cc6327cb66f8083a32f1
britton <britton@126.com>
2021-08-09 16:26:21 +0800

    完善canal配置

--- leaf-base-canal/src/main/java/com/geoxus/listener/GXCanalRabbitMQListener.java
+++ leaf-base-canal/src/main/java/com/geoxus/listener/GXCanalRabbitMQListener.java
@@ -1,24 +1,30 @@
 package com.geoxus.listener;
 
+import com.geoxus.constant.CanalConstant;
 import com.geoxus.service.GXCanalMessageParseService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
-@Configuration
 @Slf4j
+@Component
 public class GXCanalRabbitMQListener {
     @Resource
     private GXCanalMessageParseService canalMessageParseService;
 
-    /*    @RabbitListener(bindings = {
-                @QueueBinding(
-                        value = @Queue(value = CanalConstant.RABBITMQ_CANAL_QUEUE_NAME, durable = "true"),
-                        exchange = @Exchange(value = CanalConstant.RABBITMQ_CANAL_EXCHANGE_NAME, type = ExchangeTypes.DIRECT),
-                        key = CanalConstant.RABBITMQ_CANAL_ROUTING_KEY
-                )
-        }, concurrency = CanalConstant.RABBITMQ_CANAL_CONCURRENCY_COUNT)*/
+    @RabbitListener(bindings = {
+            @QueueBinding(
+                    value = @Queue(value = "${canal.queue}", durable = "true"),
+                    exchange = @Exchange(value = CanalConstant.RABBITMQ_CANAL_EXCHANGE_NAME, type = ExchangeTypes.DIRECT),
+                    key = CanalConstant.RABBITMQ_CANAL_ROUTING_KEY
+            )
+    }, concurrency = CanalConstant.RABBITMQ_CANAL_CONCURRENCY_COUNT)
     public void listener(String message) {
         canalMessageParseService.parseMessage(message);
     }
