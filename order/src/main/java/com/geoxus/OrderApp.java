a72c071a4256974ca33707db5ce5a467f0728dc7
chenzhongjian <britton@126.com>
2021-09-18 12:12:47 +0800

    重构

--- order/src/main/java/com/geoxus/OrderApp.java
+++ order/src/main/java/com/geoxus/OrderApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class OrderApp {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(OrderApp.class, args);
     }
 }
