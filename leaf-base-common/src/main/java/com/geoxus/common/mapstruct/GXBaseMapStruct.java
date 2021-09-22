d70c5e0f7f9dd2ebadb757d8a6a63b5116892333
chenzhongjian <britton@126.com>
2021-09-22 11:54:30 +0800

    *fix

--- leaf-base-common/src/main/java/com/geoxus/common/mapstruct/GXBaseMapStruct.java
+++ leaf-base-common/src/main/java/com/geoxus/common/mapstruct/GXBaseMapStruct.java
@@ -3,10 +3,13 @@ package com.geoxus.common.mapstruct;
 import com.geoxus.common.dto.GXBaseDto;
 
 /**
+ * 标记接口
+ *
  * @author britton <britton@126.com>
  * @version 1.0
  * @since 2020-09-26
  */
+@SuppressWarnings("all")
 public interface GXBaseMapStruct<D extends GXBaseDto, E> {
 
 }
