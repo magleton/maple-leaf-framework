970997c0373085f04552e77d2df276bef9c0bcc8
britton <britton@126.com>
2021-09-26 16:29:16 +0800

    + 新增校验分组

---
+++ leaf-base-common/src/main/java/com/geoxus/common/validator/validator/group/AddGroup.java
@@ -0,0 +1,9 @@
+package com.geoxus.common.validator.validator.group;
+
+/**
+ * 新增数据 Group
+ *
+ * @author britton britton@126.com
+ */
+public interface AddGroup {
+}
---
+++ leaf-base-common/src/main/java/com/geoxus/common/validator/validator/group/UpdateGroup.java
@@ -0,0 +1,9 @@
+package com.geoxus.common.validator.validator.group;
+
+/**
+ * 更新数据 Group
+ *
+ * @author britton britton@126.com
+ */
+public interface UpdateGroup {
+}
