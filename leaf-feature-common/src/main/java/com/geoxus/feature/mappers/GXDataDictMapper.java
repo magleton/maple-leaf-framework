7ec0f415540c1a6cdea17d00c5e076ebb709f15d
britton <britton@126.com>
2021-09-30 17:53:41 +0800

    *fix 重构

--- leaf-feature-common/src/main/java/com/geoxus/feature/mappers/GXDataDictMapper.java
+++ leaf-feature-common/src/main/java/com/geoxus/feature/mappers/GXDataDictMapper.java
@@ -1,6 +1,7 @@
 package com.geoxus.feature.mappers;
 
 import cn.hutool.core.lang.Dict;
+import com.geoxus.core.mapper.GXBaseMapper;
 import com.geoxus.feature.builder.GXDataDictBuilder;
 import com.geoxus.feature.entities.GXDataDictEntity;
 import org.apache.ibatis.annotations.Mapper;
@@ -9,7 +10,7 @@ import org.apache.ibatis.annotations.SelectProvider;
 import java.util.List;
 
 @Mapper
-public interface GXDataDictMapper extends GXFrameworkBaseMapper<GXDataDictEntity> {
+public interface GXDataDictMapper extends GXBaseMapper<GXDataDictEntity> {
     @SelectProvider(type = GXDataDictBuilder.class, method = "listOrSearchPage")
     List<Dict> listOrSearchPage(Dict condition);
 }
--- leaf-feature-common/src/main/java/com/geoxus/feature/mappers/GXRegionMapper.java
+++ leaf-feature-common/src/main/java/com/geoxus/feature/mappers/GXRegionMapper.java
@@ -1,13 +1,14 @@
 package com.geoxus.feature.mappers;
 
 import cn.hutool.core.lang.Dict;
+import com.geoxus.core.mapper.GXBaseMapper;
 import com.geoxus.feature.builder.GXRegionBuilder;
 import com.geoxus.feature.entities.GXRegionEntity;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.SelectProvider;
 
 @Mapper
-public interface GXRegionMapper extends GXFrameworkBaseMapper<GXRegionEntity> {
+public interface GXRegionMapper extends GXBaseMapper<GXRegionEntity> {
     @SelectProvider(type = GXRegionBuilder.class, method = "areaInfo")
     Dict areaInfo(Dict param);
 }
