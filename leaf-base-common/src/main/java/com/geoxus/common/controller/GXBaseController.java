9901583d7d614043cf2dd9b4867b6b5ea85a5579
britton <britton@126.com>
2021-10-11 18:13:04 +0800

    *fix 重构

--- leaf-base-common/src/main/java/com/geoxus/common/controller/GXBaseController.java
+++ leaf-base-common/src/main/java/com/geoxus/common/controller/GXBaseController.java
@@ -1,5 +1,7 @@
 package com.geoxus.common.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.geoxus.common.dto.protocol.res.GXPaginationProtocol;
 import com.geoxus.common.util.GXHttpContextUtils;
 
 public interface GXBaseController {
@@ -12,5 +14,15 @@ public interface GXBaseController {
      */
     default long getUserIdFromToken(String tokenName, String tokenIdName) {
         return GXHttpContextUtils.getUserIdFromToken(tokenName, tokenIdName);
+    }
+
+    /**
+     * 从token中获取用户ID
+     *
+     * @param riPage 分页对象
+     * @return Long
+     */
+    default <R> GXPaginationProtocol<R> generateGXPaginationProtocol(IPage<R> riPage) {
+        return new GXPaginationProtocol<>(riPage.getRecords(), riPage.getTotal(), riPage.getSize(), riPage.getCurrent());
     }
 }
