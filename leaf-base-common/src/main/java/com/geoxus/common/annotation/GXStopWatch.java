62fcb6ef4b4903e553f39b7de208484ecfd386f1
britton <britton@126.com>
2021-10-19 17:12:56 +0800

    + 新增测量方法运行时间及日志记录

---
+++ leaf-base-common/src/main/java/com/geoxus/common/annotation/GXStopWatch.java
@@ -0,0 +1,15 @@
+package com.geoxus.common.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author britton@126.com
+ * @since 2021-10-19 15:20
+ * <p>
+ * 测量一个方法运行的时间
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface GXStopWatch {
+}
