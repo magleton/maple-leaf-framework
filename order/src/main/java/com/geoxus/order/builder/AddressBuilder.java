a7f3833687626be64b23678e08431a30c70dd53d
britton <britton@126.com>
2021-10-24 11:23:27 +0800

    *fix 代码重构

--- order/src/main/java/com/geoxus/order/builder/AddressBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.geoxus.order.builder;
-
-import com.geoxus.order.builder.dto.AddressDto;
-
-public class AddressBuilder {
-    public String getListByCondition(AddressDto dto) {
-        return "select * from address";
-    }
-}
