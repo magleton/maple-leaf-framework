2ae4d715b5adc34853dc0c753ac48d0ce9518a6b
britton <britton@126.com>
2021-11-03 18:26:03 +0800

    *fix 新增如redisson的基本操作服务类

---
+++ leaf-base-redisson/src/main/java/cn/maple/redisson/services/impl/GXRedissonCacheServiceImpl.java
@@ -0,0 +1,107 @@
+package cn.maple.redisson.services.impl;
+
+import cn.maple.redisson.services.GXRedissonCacheService;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@Slf4j
+public class GXRedissonCacheServiceImpl implements GXRedissonCacheService {
+    @Resource
+    private RedissonClient redissonClient;
+
+    /**
+     * 设置缓存 有过期时间
+     *
+     * @param bucketName 数据桶的名字 来源于 CacheConstant
+     * @param key        缓存key
+     * @param value      缓存值
+     * @param expired    过期时间
+     * @param timeUnit   时间单位
+     * @return 是否成功
+     */
+    @Override
+    public Object setCache(String bucketName, String key, Object value, int expired, TimeUnit timeUnit) {
+        return redissonClient.getMapCache(bucketName).put(key, value, expired, timeUnit);
+    }
+
+    /**
+     * 设置缓存 没有过期时间
+     *
+     * @param bucketName 数据桶的名字 来源于 CacheConstant
+     * @param key        缓存key
+     * @param value      缓存值
+     * @return 是否成功
+     */
+    @Override
+    public Object setCache(String bucketName, String key, String value) {
+        return redissonClient.getMapCache(bucketName).put(key, value);
+    }
+
+    /**
+     * 获取缓存数据
+     *
+     * @param bucketName 数据桶的名字 来源于 CacheConstant
+     * @param key        缓存key
+     * @return 缓存值
+     */
+    @Override
+    public Object getCache(String bucketName, String key) {
+        return redissonClient.getMapCache(bucketName).get(key);
+    }
+
+    /**
+     * 删除指定的缓存数据
+     *
+     * @param bucketName 数据桶的名字 来源于 CacheConstant
+     * @param key        缓存key
+     * @return 已经删除的数据
+     */
+    @Override
+    public Object deleteCache(String bucketName, String key) {
+        return redissonClient.getMapCache(bucketName).remove(key);
+    }
+
+    /**
+     * 获取缓存剩余有效时长
+     *
+     * @param bucketName 缓存桶名字
+     * @param keyName    缓存key
+     * @return 剩余有效时长
+     */
+    @Override
+    public Long getCacheRemainTimeToLive(String bucketName, String keyName) {
+        return redissonClient.getMapCache(bucketName).remainTimeToLive(keyName);
+    }
+
+    /**
+     * 跟新缓存有效时长
+     *
+     * @param bucketName       缓存桶名字
+     * @param keyName          缓存key
+     * @param expired          需要增加的过期时间 单位:秒
+     * @param refreshThreshold 刷新token缓存时间的阈值 如果小于该值 则刷新缓存的过期时间
+     * @return 是否成功
+     */
+    @Override
+    public boolean updateCacheExpiredTime(String bucketName, String keyName, Integer expired, Integer refreshThreshold) {
+        Object o = redissonClient.getMapCache(bucketName).get(keyName);
+        if (Objects.isNull(o)) {
+            log.error("{}-{}缓存不存在", bucketName, keyName);
+            return false;
+        }
+        long ttl = redissonClient.getMapCache(bucketName).remainTimeToLive(keyName);
+        if (ttl <= refreshThreshold) {
+            // <code>true</code> if key is a new key in the hash and value was set.
+            // <code>false</code> if key already exists in the hash and the value was updated.
+            // fastPut方法在此处会返回false 所以需要取反
+            return !redissonClient.getMapCache(bucketName).fastPut(keyName, o, expired, TimeUnit.SECONDS);
+        }
+        return true;
+    }
+}
