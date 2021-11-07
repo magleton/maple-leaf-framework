adb52530e04b0a17ab1543036757d8ea2d7e52e3
britton <britton@126.com>
2021-11-07 11:47:29 +0800

    *fix 接口新增功能

--- leaf-base-framework/src/main/java/cn/maple/core/framework/ddd/repository/GXBaseRepository.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/ddd/repository/GXBaseRepository.java
@@ -1,5 +1,7 @@
 package cn.maple.core.framework.ddd.repository;
 
+import cn.maple.core.framework.ddd.presenter.GXBasePresenter;
+import cn.maple.core.framework.dto.inner.res.GXPaginationResDto;
 import com.google.common.collect.Table;
 
 import java.util.List;
@@ -15,4 +17,38 @@ public interface GXBaseRepository {
      * @return 列表
      */
     <R> List<R> all(Set<String> columns, Table<String, String, Object> condition);
+
+    /**
+     * 根据条件获取数据
+     *
+     * @param whereCondition 查询条件
+     * @param <R>            数据类型
+     * @return R 返回数据
+     */
+    <R> R findByCondition(Table<String, String, Object> whereCondition);
+
+    /**
+     * 设置presenter对象
+     *
+     * @param presenter presenter对象
+     */
+    GXBaseRepository setPresenter(GXBasePresenter presenter);
+
+    /**
+     * 分页数据
+     *
+     * @param limit   限制条数
+     * @param columns 需要的数据列
+     * @param <R>     类型
+     * @return 分页对象
+     */
+    <R> GXPaginationResDto<R> paginate(Integer limit, Set<String> columns);
+
+    /**
+     * 根据条件删除
+     *
+     * @param whereCondition 删除条件
+     * @return 影响行数
+     */
+    int deleteWhere(Table<String, String, Object> whereCondition);
 }
