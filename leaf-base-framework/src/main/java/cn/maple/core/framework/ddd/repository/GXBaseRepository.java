173ce0d25751e3b75613f2314751a459c93c0632
britton <britton@126.com>
2021-11-07 15:09:58 +0800

    *fix 接口新增功能

--- leaf-base-framework/src/main/java/cn/maple/core/framework/ddd/repository/GXBaseRepository.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/ddd/repository/GXBaseRepository.java
@@ -1,86 +1,72 @@
 package cn.maple.core.framework.ddd.repository;
 
-import cn.maple.core.framework.ddd.presenter.GXBasePresenter;
 import cn.maple.core.framework.dto.inner.req.GXBaseReqDto;
+import cn.maple.core.framework.dto.inner.res.GXBaseResDto;
 import cn.maple.core.framework.dto.inner.res.GXPaginationResDto;
 import com.google.common.collect.Table;
 
 import java.util.List;
 import java.util.Set;
 
-public interface GXBaseRepository {
+public interface GXBaseRepository<E extends GXBaseReqDto, R extends GXBaseResDto> {
     /**
      * 保存数据
      *
      * @param saveData 需要保存的数据
-     * @param <R>      返回数据类型
      * @return R
      */
-    <R> R create(GXBaseReqDto saveData);
+    Integer create(E saveData);
 
     /**
      * 保存数据
      *
      * @param updateData 需要更新的数据
-     * @param <R>        返回数据类型
      * @return R
      */
-    <R> R update(GXBaseReqDto updateData);
+    Integer update(E updateData);
 
     /**
      * 保存数据
      *
      * @param data 需要更新或者保存的数据
-     * @param <R>  返回数据类型
      * @return R
      */
-    <R> R updateOrCreate(GXBaseReqDto data);
+    Integer updateOrCreate(E data);
 
     /**
      * 获取所有数据
      *
      * @param columns   需要获取的列
      * @param condition 条件
-     * @param <R>       数据的类型
      * @return 列表
      */
-    <R> List<R> all(Set<String> columns, Table<String, String, Object> condition);
+    List<R> all(Set<String> columns, Table<String, String, Object> condition);
 
     /**
      * 获取第一条数据
      *
      * @param columns   需要获取的列
      * @param condition 条件
-     * @param <R>       数据的类型
      * @return 列表
      */
-    <R> R first(Set<String> columns, Table<String, String, Object> condition);
+    R first(Set<String> columns, Table<String, String, Object> condition);
 
     /**
      * 根据条件获取数据
      *
      * @param whereCondition 查询条件
-     * @param <R>            数据类型
      * @return R 返回数据
      */
-    <R> R findByCondition(Table<String, String, Object> whereCondition);
-
-    /**
-     * 设置presenter对象
-     *
-     * @param presenter presenter对象
-     */
-    GXBaseRepository setPresenter(GXBasePresenter presenter);
+    R findByCondition(Table<String, String, Object> whereCondition);
 
     /**
      * 分页数据
      *
      * @param limit   限制条数
      * @param columns 需要的数据列
-     * @param <R>     类型
      * @return 分页对象
      */
-    <R> GXPaginationResDto<R> paginate(Integer limit, Set<String> columns);
+    GXPaginationResDto<R> paginate(Integer limit, Set<String> columns);
 
     /**
      * 根据条件删除
