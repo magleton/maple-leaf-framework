4d011909d5681e585437f60f060c188869aa607e
britton <britton@126.com>
2021-12-06 15:04:08 +0800

    *fix 新增skywalking及修改日志

--- leaf-base-framework/src/main/java/cn/maple/core/framework/service/impl/GXLocalLoggerServiceImpl.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/service/impl/GXLocalLoggerServiceImpl.java
@@ -1,24 +1,20 @@
-package cn.maple.core.framework.util;
+package cn.maple.core.framework.service.impl;
 
 import cn.hutool.core.text.CharSequenceUtil;
 import cn.hutool.json.JSONUtil;
 import cn.maple.core.framework.constant.GXCommonConstant;
+import cn.maple.core.framework.service.GXLoggerService;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
-public class GXLoggerUtils {
+@Service
+@ConditionalOnMissingClass(value = "org.apache.skywalking.apm.toolkit.trace.ActiveSpan")
+public class GXLocalLoggerServiceImpl implements GXLoggerService {
     /**
-     * 日志对象
-     */
-    private static final Logger LOG = LoggerFactory.getLogger(GXLoggerUtils.class);
-
-    private GXLoggerUtils() {
-    }
-
-    /**
-     * 记录日志
+     * 记录Info日志
      *
      * @param logger 日志对象
      * @param desc   描述信息
@@ -26,7 +22,8 @@ public class GXLoggerUtils {
      * @author britton
      * @since 2021-10-19 17:40
      */
-    public static void logInfo(Logger logger, String desc, Object data) {
+    @Override
+    public void logInfo(Logger logger, String desc, Object data) {
         String threadName = Thread.currentThread().getName();
         String jsonStr = "";
         if (Objects.nonNull(data)) {
@@ -37,7 +34,7 @@ public class GXLoggerUtils {
     }
 
     /**
-     * 记录日志
+     * 记录Debug日志
      *
      * @param logger 日志对象
      * @param desc   描述信息
@@ -45,7 +42,8 @@ public class GXLoggerUtils {
      * @author britton
      * @since 2021-10-19 17:40
      */
-    public static void logDebug(Logger logger, String desc, Object data) {
+    @Override
+    public void logDebug(Logger logger, String desc, Object data) {
         String threadName = Thread.currentThread().getName();
         String jsonStr = "";
         if (Objects.nonNull(data)) {
@@ -56,7 +54,7 @@ public class GXLoggerUtils {
     }
 
     /**
-     * 记录日志
+     * 记录Error日志
      *
      * @param logger 日志对象
      * @param desc   描述信息
@@ -64,7 +62,8 @@ public class GXLoggerUtils {
      * @author britton
      * @since 2021-10-19 17:40
      */
-    public static void logError(Logger logger, String desc, Object data) {
+    @Override
+    public void logError(Logger logger, String desc, Object data) {
         String threadName = Thread.currentThread().getName();
         String jsonStr = "";
         if (Objects.nonNull(data)) {
@@ -75,7 +74,7 @@ public class GXLoggerUtils {
     }
 
     /**
-     * 记录日志
+     * 记录Error日志
      *
      * @param logger 日志对象
      * @param desc   描述信息
@@ -83,14 +82,15 @@ public class GXLoggerUtils {
      * @author britton
      * @since 2021-10-19 17:40
      */
-    public static void logError(Logger logger, String desc, Throwable t) {
+    @Override
+    public void logError(Logger logger, String desc, Throwable t) {
         String threadName = Thread.currentThread().getName();
         String format = CharSequenceUtil.format("{} : {}", threadName, desc);
         logger.error(format, t);
     }
 
     /**
-     * 记录日志
+     * 记录Warning日志
      *
      * @param logger 日志对象
      * @param desc   描述信息
@@ -98,7 +98,8 @@ public class GXLoggerUtils {
      * @author britton
      * @since 2021-10-19 17:40
      */
-    public static void logWarn(Logger logger, String desc, Object data) {
+    @Override
+    public void logWarn(Logger logger, String desc, Object data) {
         String threadName = Thread.currentThread().getName();
         String jsonStr = "";
         if (Objects.nonNull(data)) {
---
+++ leaf-base-skywalking/src/main/java/cn/maple/skywalking/service/impl/GXSkyWalkingLoggerServiceImpl.java
@@ -0,0 +1,113 @@
+package cn.maple.skywalking.service.impl;
+
+import cn.hutool.core.text.CharSequenceUtil;
+import cn.hutool.json.JSONUtil;
+import cn.maple.core.framework.constant.GXCommonConstant;
+import cn.maple.core.framework.service.GXLoggerService;
+import org.apache.skywalking.apm.toolkit.trace.ActiveSpan;
+import org.slf4j.Logger;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+@ConditionalOnClass(name = "org.apache.skywalking.apm.toolkit.trace.ActiveSpan")
+public class GXSkyWalkingLoggerServiceImpl implements GXLoggerService {
+    /**
+     * 记录Info日志
+     *
+     * @param logger 日志对象
+     * @param desc   描述信息
+     * @param data   记录数据
+     * @author britton
+     * @since 2021-10-19 17:40
+     */
+    @Override
+    public void logInfo(Logger logger, String desc, Object data) {
+        String threadName = Thread.currentThread().getName();
+        String jsonStr = "";
+        if (Objects.nonNull(data)) {
+            jsonStr = JSONUtil.toJsonStr(data);
+        }
+        String s = CharSequenceUtil.format(GXCommonConstant.LOGGER_FORMAT, threadName, desc, jsonStr);
+        ActiveSpan.info(s);
+    }
+
+    /**
+     * 记录Debug日志
+     *
+     * @param logger 日志对象
+     * @param desc   描述信息
+     * @param data   记录数据
+     * @author britton
+     * @since 2021-10-19 17:40
+     */
+    @Override
+    public void logDebug(Logger logger, String desc, Object data) {
+        String threadName = Thread.currentThread().getName();
+        String jsonStr = "";
+        if (Objects.nonNull(data)) {
+            jsonStr = JSONUtil.toJsonStr(data);
+        }
+        String s = CharSequenceUtil.format(GXCommonConstant.LOGGER_FORMAT, threadName, desc, jsonStr);
+        ActiveSpan.debug(s);
+    }
+
+    /**
+     * 记录Error日志
+     *
+     * @param logger 日志对象
+     * @param desc   描述信息
+     * @param data   记录数据
+     * @author britton
+     * @since 2021-10-19 17:40
+     */
+    @Override
+    public void logError(Logger logger, String desc, Object data) {
+        String threadName = Thread.currentThread().getName();
+        String jsonStr = "";
+        if (Objects.nonNull(data)) {
+            jsonStr = JSONUtil.toJsonStr(data);
+        }
+        String s = CharSequenceUtil.format(GXCommonConstant.LOGGER_FORMAT, threadName, desc, jsonStr);
+        ActiveSpan.error(s);
+    }
+
+    /**
+     * 记录Error日志
+     *
+     * @param logger 日志对象
+     * @param desc   描述信息
+     * @param t      异常信息
+     * @author britton
+     * @since 2021-10-19 17:40
+     */
+    @Override
+    public void logError(Logger logger, String desc, Throwable t) {
+        String threadName = Thread.currentThread().getName();
+        String s = CharSequenceUtil.format("{} : {}", threadName, desc);
+        ActiveSpan.error(s);
+        ActiveSpan.error(t);
+    }
+
+    /**
+     * 记录Warning日志
+     *
+     * @param logger 日志对象
+     * @param desc   描述信息
+     * @param data   记录数据
+     * @author britton
+     * @since 2021-10-19 17:40
+     */
+    @Override
+    public void logWarn(Logger logger, String desc, Object data) {
+        String threadName = Thread.currentThread().getName();
+        String jsonStr = "";
+        if (Objects.nonNull(data)) {
+            jsonStr = JSONUtil.toJsonStr(data);
+        }
+        String s = CharSequenceUtil.format(GXCommonConstant.LOGGER_FORMAT, threadName, desc, jsonStr);
+        ActiveSpan.info(s);
+    }
+}
