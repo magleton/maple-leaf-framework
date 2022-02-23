3fd08d392707180a8fb0bd68fdce98eefed5ff6c
britton <britton@126.com>
2022-02-23 18:04:21 +0800

    *fix 新增RedissonModule模块

--- leaf-base-redisson-module/src/main/java/cn/maple/redisson/module/GXRedisModuleConfig.java
+++ leaf-base-redisson-module/src/main/java/cn/maple/redisson/module/GXRedisModuleConfig.java
@@ -1,30 +1,26 @@
-package cn.maple.redisson.config;
+package cn.maple.redisson.module;
 
 import io.github.dengliming.redismodule.redisearch.client.RediSearchClient;
 import io.github.dengliming.redismodule.redisjson.client.RedisJSONClient;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.config.Config;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
-@Configuration
+@Component
 @Slf4j
-@ConditionalOnClass(name = "io.github.dengliming.redismodule.common.util.ArgsUtil")
 public class GXRedisModuleConfig {
     @Resource
     private Config config;
 
     @Bean
-    @ConditionalOnClass(name = "io.github.dengliming.redismodule.redisjson.client.RedisJSONClient")
     public RedisJSONClient redisJSONClient() {
         return new RedisJSONClient(config);
     }
 
     @Bean
-    @ConditionalOnClass(name = "io.github.dengliming.redismodule.redisearch.client.RediSearchClient")
     public RediSearchClient rediSearchClient() {
         return new RediSearchClient(config);
     }
