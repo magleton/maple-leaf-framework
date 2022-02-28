11f8ca7021e0e2241e1daa6ca4f74827c1662090
britton <britton@126.com>
2022-02-28 11:20:04 +0800

    *fix 完善rediSearch基础功能

--- leaf-base-redisson-module/src/main/java/cn/maple/redisson/module/service/GXRediSearchService.java
+++ leaf-base-redisson-module/src/main/java/cn/maple/redisson/module/service/GXRediSearchService.java
@@ -1,17 +1,17 @@
 package cn.maple.redisson.module.service;
 
-import cn.maple.core.framework.dto.inner.GXBaseSEParamInnerDto;
-import cn.maple.core.framework.dto.res.GXBaseResDto;
+import cn.hutool.core.lang.Dict;
+import cn.maple.redisson.module.dto.GXBaseRediSeachParamInnerDto;
 
 import java.util.List;
 
 @SuppressWarnings("all")
-public interface GXRediSearchService<R extends GXBaseResDto> {
+public interface GXRediSearchService {
     /**
      * 按照条件进行搜索
      *
      * @param paramInnerDto 搜索条件
      * @return List
      */
-    List<R> search(GXBaseSEParamInnerDto paramInnerDto);
+    List<Dict> search(GXBaseRediSeachParamInnerDto paramInnerDto);
 }
---
+++ leaf-base-redisson-module/src/main/java/cn/maple/redisson/module/service/impl/GXRediSearchServiceImpl.java
@@ -0,0 +1,27 @@
+package cn.maple.redisson.module.service.impl;
+
+import cn.hutool.core.lang.Dict;
+import cn.maple.redisson.module.dto.GXBaseRediSeachParamInnerDto;
+import cn.maple.redisson.module.repository.GXRediSearchRepository;
+import cn.maple.redisson.module.service.GXRediSearchService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class GXRediSearchServiceImpl implements GXRediSearchService {
+    @Resource
+    private GXRediSearchRepository rediSearchRepository;
+
+    /**
+     * 按照条件进行搜索
+     *
+     * @param paramInnerDto 搜索条件
+     * @return List
+     */
+    @Override
+    public List<Dict> search(GXBaseRediSeachParamInnerDto paramInnerDto) {
+        return rediSearchRepository.search(paramInnerDto, Dict.class);
+    }
+}
