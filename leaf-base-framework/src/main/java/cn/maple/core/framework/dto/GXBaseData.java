66946305d5115abad645dda2408c08c9cf9f3295
britton <britton@126.com>
2022-03-01 18:14:02 +0800

    *fix 新增JSON转换函数

--- leaf-base-framework/src/main/java/cn/maple/core/framework/dto/GXBaseData.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/dto/GXBaseData.java
@@ -1,8 +1,12 @@
 package cn.maple.core.framework.dto;
 
 import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.json.JSONUtil;
+import cn.maple.core.framework.util.GXCommonUtils;
 
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
 
 public abstract class GXBaseData implements Serializable {
     /**
@@ -49,5 +53,33 @@ public abstract class GXBaseData implements Serializable {
         this.beforeRepair();
         this.verify();
         this.afterRepair();
+    }
+
+    /**
+     * 将JSON数组转换为指定的对象列表
+     *
+     * @param jsonArray   JSON数组
+     * @param targetClass 目标对象
+     * @return 列表
+     */
+    protected <E> List<E> convertJsonArrayToTarget(String jsonArray, Class<E> targetClass) {
+        if (JSONUtil.isJsonArray(jsonArray)) {
+            return JSONUtil.toList(jsonArray, targetClass);
+        }
+        return Collections.emptyList();
+    }
+
+    /**
+     * 将JSON数组转换为指定的对象列表
+     *
+     * @param jsonObject  JSON对象
+     * @param targetClass 目标对象
+     * @return 列表
+     */
+    protected <E> E convertJsonObjectToTarget(String jsonObject, Class<E> targetClass) {
+        if (JSONUtil.isJsonObj(jsonObject)) {
+            return JSONUtil.toBean(jsonObject, targetClass);
+        }
+        return GXCommonUtils.getClassDefaultValue(targetClass);
     }
 }
