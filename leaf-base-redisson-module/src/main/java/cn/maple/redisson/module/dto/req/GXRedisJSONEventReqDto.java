5ff5aad96c016b0893370aa9726b522a461c06b7
britton <britton@126.com>
2022-03-06 14:24:10 +0800

    *fix 完善Redis模块接口

--- leaf-base-redisson-module/src/main/java/cn/maple/redisson/module/dto/req/GXRedisJSONEventReqDto.java
+++ leaf-base-redisson-module/src/main/java/cn/maple/redisson/module/dto/req/GXRedisJSONEventReqDto.java
@@ -1,5 +1,6 @@
 package cn.maple.redisson.module.dto.req;
 
+import cn.hutool.core.lang.Dict;
 import cn.maple.core.framework.dto.req.GXBaseReqDto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -7,4 +8,18 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class GXRedisJSONEventReqDto extends GXBaseReqDto {
+    /**
+     * Redis的key
+     */
+    private String key;
+
+    /**
+     * JSON的路径
+     */
+    private String path;
+
+    /**
+     * JSON数据
+     */
+    private Dict data;
 }
