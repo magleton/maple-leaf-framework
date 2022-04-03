40ec1f63756c37c819417c47245ef1b6e9f00be8
枫叶思源 <britton@126.com>
2022-04-03 16:32:06 +0800

    +add 新增ResDto的基础类  区分出API和数据库查询

---
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/api/dto/GXBaseApiResDto.java
@@ -0,0 +1,10 @@
+package cn.maple.core.framework.api.dto;
+
+import cn.maple.core.framework.dto.res.GXBaseResDto;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class GXBaseApiResDto extends GXBaseResDto {
+}
