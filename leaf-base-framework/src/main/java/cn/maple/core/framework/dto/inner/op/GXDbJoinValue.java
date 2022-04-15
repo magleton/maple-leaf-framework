7c708140d46934ad443e811f7efc9d0b56f353c6
枫叶思源 <britton@126.com>
2022-04-15 13:39:17 +0800

    *feat 重构

--- leaf-base-framework/src/main/java/cn/maple/core/framework/dto/inner/op/GXDbJoinValue.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/dto/inner/op/GXDbJoinValue.java
@@ -2,15 +2,21 @@ package cn.maple.core.framework.dto.inner.op;
 
 @SuppressWarnings("all")
 public abstract class GXDbJoinValue extends GXDbJoinOp {
+    /**
+     * 表别名
+     */
     private final String tableNameAlias;
 
+    /**
+     * 表字段
+     */
     private final String fieldName;
-    
+
     /**
      * 字段的值
      * 用于查询固定值的场景
      */
-    protected Object fieldValue;
+    private Object fieldValue;
 
     public GXDbJoinValue(String tableNameAlias, String fieldName, Object fieldValue) {
         this.tableNameAlias = tableNameAlias;
