fea098bd8e6f9dcbca8030c03cbc95f33798f38e
枫叶思源 <britton@126.com>
2022-04-24 18:37:05 +0800

    *feat 小修复

--- leaf-base-datasource/src/main/java/cn/maple/core/datasource/service/GXMyBatisBaseService.java
+++ leaf-base-datasource/src/main/java/cn/maple/core/datasource/service/GXMyBatisBaseService.java
@@ -500,7 +500,10 @@ public interface GXMyBatisBaseService<P extends GXMyBatisRepository<M, T, D, ID>
     default CopyOptions getCopyOptions(GXBaseQueryParamInnerDto queryParamInnerDto) {
         CopyOptions copyOptions = Optional.ofNullable(queryParamInnerDto.getCopyOptions()).orElse(CopyOptions.create());
         copyOptions.setConverter((type, value) -> {
-            if (Dict.class.isAssignableFrom((Class<?>) type) && JSONUtil.isTypeJSON(value.toString())) {
+            if (JSONUtil.isTypeJSON(value.toString())) {
+                if (JSONUtil.isTypeJSONArray(value.toString())) {
+                    return JSONUtil.toList(value.toString(), Dict.class);
+                }
                 return JSONUtil.toBean(value.toString(), Dict.class);
             }
             return Convert.convertWithCheck(type, value, null, false);
