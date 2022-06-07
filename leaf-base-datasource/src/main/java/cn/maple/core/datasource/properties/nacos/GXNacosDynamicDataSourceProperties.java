8e3627d7f233c507db6e7957e5efda90557672f8
枫叶思源 <britton@126.com>
2022-06-07 20:57:32 +0800

    *feat 完善数据源对与Spring Cloud的支持，包括对seata的支持

--- leaf-base-datasource/src/main/java/cn/maple/core/datasource/properties/nacos/GXNacosDynamicDataSourceProperties.java
+++ leaf-base-datasource/src/main/java/cn/maple/core/datasource/properties/nacos/GXNacosDynamicDataSourceProperties.java
@@ -2,12 +2,13 @@ package cn.maple.core.datasource.properties.nacos;
 
 import cn.maple.core.datasource.properties.GXDataSourceProperties;
 import cn.maple.core.datasource.properties.GXDynamicDataSourceProperties;
-import com.alibaba.nacos.api.config.ConfigType;
-import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
+import com.alibaba.nacos.api.annotation.NacosProperties;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.LinkedHashMap;
@@ -22,7 +23,14 @@ import java.util.Map;
 @SuppressWarnings("all")
 @EqualsAndHashCode(callSuper = true)
 @ConditionalOnClass(name = {"com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties"})
-@NacosConfigurationProperties(groupId = "${nacos.config.group:DEFAULT_GROUP}", prefix = "dynamic", dataId = "datasource.yml", autoRefreshed = true, type = ConfigType.YAML)
+@NacosPropertySource(dataId = "datasource.yml",
+        groupId = "${spring.cloud.nacos.config.group:${nacos.config.group:}}",
+        properties = @NacosProperties(
+                serverAddr = "${spring.cloud.nacos.config.server-addr:${nacos.config.server-addr:}}",
+                namespace = "${spring.cloud.nacos.config.namespace:${nacos.config.namespace:}}",
+                username = "${spring.cloud.nacos.username:${nacos.config.username:}}",
+                password = "${spring.cloud.nacos.password:${nacos.config.password:}}"))
+@ConfigurationProperties(prefix = "dynamic")
 public class GXNacosDynamicDataSourceProperties extends GXDynamicDataSourceProperties {
     private Map<String, GXDataSourceProperties> datasource = new LinkedHashMap<>();
 
