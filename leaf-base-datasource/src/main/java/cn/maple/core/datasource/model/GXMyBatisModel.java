8bd1e618121bea4fa1e1ddea042c6da9c18e08ab
塵渊 <britton@126.com>
2022-08-04 10:29:31 +0800

    feat 基础模型新增固定字段

--- leaf-base-datasource/src/main/java/cn/maple/core/datasource/model/GXMyBatisModel.java
+++ leaf-base-datasource/src/main/java/cn/maple/core/datasource/model/GXMyBatisModel.java
@@ -1,11 +1,23 @@
 package cn.maple.core.datasource.model;
 
 import cn.maple.core.framework.model.GXBaseModel;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class GXMyBatisModel extends GXBaseModel {
+    @TableField(fill = FieldFill.INSERT)
+    private Integer createdAt;
 
+    @TableField(fill = FieldFill.UPDATE)
+    private Integer updatedAt;
+
+    @TableField(fill = FieldFill.INSERT)
+    private String createdBy;
+
+    @TableField(fill = FieldFill.UPDATE)
+    private String updatedBy;
 }
