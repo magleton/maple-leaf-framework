bc219b0b2d509cf5e1c0e8ee88717be87d9c0d32
塵渊 <britton@126.com>
2022-11-03 09:22:26 +0800

    feat 还原对模型层扩展字段的处理

--- leaf-base-datasource/src/main/java/cn/maple/core/datasource/model/GXMyBatisModel.java
+++ leaf-base-datasource/src/main/java/cn/maple/core/datasource/model/GXMyBatisModel.java
@@ -1,11 +1,10 @@
 package cn.maple.core.datasource.model;
 
-import cn.hutool.core.convert.Convert;
-import cn.hutool.core.util.ObjectUtil;
-import cn.hutool.json.JSONUtil;
+import cn.hutool.core.lang.Dict;
 import cn.maple.core.framework.model.GXBaseModel;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -24,33 +23,6 @@ public class GXMyBatisModel extends GXBaseModel {
     @TableField(fill = FieldFill.UPDATE)
     private String updatedBy;
 
-    /**
-     * 扩展字段
-     */
-    private Object ext;
-
-    /**
-     * 获取扩展字符串
-     *
-     * @return JSON
-     */
-    public String getExt() {
-        return Convert.toStr(ext);
-    }
-
-    /**
-     * 将传入的扩展数据转换成JSON字符串
-     *
-     * @param ext 扩展数据
-     */
-    public void setExt(Object ext) {
-        if (ObjectUtil.isNull(ext)) {
-            ext = "{}";
-        }
-        if (String.class.isAssignableFrom(ext.getClass()) && JSONUtil.isTypeJSON(ext.toString())) {
-            this.ext = ext;
-        } else {
-            this.ext = JSONUtil.toJsonStr(ext);
-        }
-    }
+    @TableField(typeHandler = JacksonTypeHandler.class)
+    private Dict ext;
 }
