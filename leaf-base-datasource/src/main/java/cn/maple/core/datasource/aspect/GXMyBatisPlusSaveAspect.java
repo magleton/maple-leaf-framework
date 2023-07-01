8e2068b8eb50b4b2bf612bd5b35770435a962e92
塵渊 <britton@126.com>
2023-07-01 17:56:52 +0800

    add 新增对MyBatis Plus的新增(save) 修改(update)的事件支持

---
+++ leaf-base-datasource/src/main/java/cn/maple/core/datasource/aspect/GXMyBatisPlusSaveAspect.java
@@ -0,0 +1,20 @@
+package cn.maple.core.datasource.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class GXMyBatisPlusSaveAspect {
+    @Around("execution(* com.baomidou.mybatisplus.extension.service.IService.save(..))")
+    public Object around(ProceedingJoinPoint point) throws Throwable {
+        // TODO 发布新增前的事件 服务可以通过监听该事件对目标对象进行新增前的最后处理
+        Object proceed = point.proceed();
+        // TODO 发布新增后的事件 服务可以通过监听该事件对目标对象进行新增后的最后处理
+        return proceed;
+    }
+}
---
+++ leaf-base-datasource/src/main/java/cn/maple/core/datasource/aspect/GXMyBatisPlusUpdateAspect.java
@@ -0,0 +1,20 @@
+package cn.maple.core.datasource.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class GXMyBatisPlusUpdateAspect {
+    @Around("execution(* com.baomidou.mybatisplus.extension.service.IService.update(..))")
+    public Object around(ProceedingJoinPoint point) throws Throwable {
+        // TODO 发布更新前的事件 服务可以通过监听该事件对目标对象进行更新前的最后处理
+        Object proceed = point.proceed();
+        // TODO 发布更新后的事件 服务可以通过监听该事件对目标对象进行更新后的最后处理
+        return proceed;
+    }
+}
