bdb7b5d6d8cf403c5e6551534be9947966a38866
塵渊 <britton@126.com>
2023-07-05 09:27:07 +0800

    feat 完善外部接口或第三方平台的标记接口注释

--- leaf-base-framework/src/main/java/cn/maple/core/framework/manager/GXExternalFeatureManager.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/manager/GXExternalFeatureManager.java
@@ -4,7 +4,7 @@ package cn.maple.core.framework.manager;
  * @author 子曦
  * 对接外部接口或第三方平台的标记接口
  * 任何需要对接外部或者第三方接口的功能都需要实现该接口
- * eg: 微信  支付宝
+ * eg: 微信  支付宝  上游系统对接
  */
 public interface GXExternalFeatureManager {
 }
