72352a1d73c18099bd91421cbd4c4ea127f25608
塵渊 <britton@126.com>
2023-07-10 11:20:21 +0800

    feat 完善ApplicationContext的设置功能

--- leaf-base-framework/src/main/java/cn/maple/core/framework/config/aware/GXApplicationContextSingleton.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/config/aware/GXApplicationContextSingleton.java
@@ -1,9 +1,19 @@
 package cn.maple.core.framework.config.aware;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 
+import java.util.Objects;
+
+@SuppressWarnings("all")
 public enum GXApplicationContextSingleton {
     INSTANCE;
+
+    /**
+     * 日志对象
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(GXApplicationContextSingleton.class);
 
     ApplicationContext applicationContext;
 
@@ -15,6 +25,9 @@ public enum GXApplicationContextSingleton {
     }
 
     public void setApplicationContext(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
+        LOG.info("GXApplicationContextSingleton类设置ApplicationContext对象被调用");
+        if (Objects.isNull(this.applicationContext)) {
+            this.applicationContext = applicationContext;
+        }
     }
 }
