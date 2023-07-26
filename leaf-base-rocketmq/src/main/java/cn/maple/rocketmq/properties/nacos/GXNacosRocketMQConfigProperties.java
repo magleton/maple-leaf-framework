48938f0c7db8d5856cc763e49b227671ac5afe88
塵渊 <britton@126.com>
2023-07-26 17:56:33 +0800

    add 新增RocketMQ模块

---
+++ leaf-base-rocketmq/src/main/java/cn/maple/rocketmq/properties/nacos/GXNacosRocketMQConfigProperties.java
@@ -0,0 +1,33 @@
+package cn.maple.rocketmq.properties.nacos;
+
+import com.alibaba.nacos.api.annotation.NacosProperties;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.autoconfigure.RocketMQProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Data
+@Slf4j
+@Component
+@Primary
+@SuppressWarnings("all")
+@EqualsAndHashCode(callSuper = true)
+@ConditionalOnClass(name = {"com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties"})
+@NacosPropertySource(dataId = "rocket-mq.yml",
+        groupId = "${spring.cloud.nacos.config.group:${nacos.config.group:}}",
+        properties = @NacosProperties(
+                serverAddr = "${spring.cloud.nacos.config.server-addr:${nacos.config.server-addr:}}",
+                namespace = "${spring.cloud.nacos.config.namespace:${nacos.config.namespace:}}",
+                username = "${spring.cloud.nacos.username:${nacos.config.username:}}",
+                password = "${spring.cloud.nacos.password:${nacos.config.password:}}"))
+@ConfigurationProperties(prefix = "rocketmq")
+public class GXNacosRocketMQConfigProperties extends RocketMQProperties {
+    public GXNacosRocketMQConfigProperties() {
+        log.info("RocketMQ数据源的配置使用的是NACOS配置");
+    }
+}
