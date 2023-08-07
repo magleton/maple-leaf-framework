1594ff835d57d0239789cd60246b787e462f0523
塵渊 <britton@126.com>
2023-08-07 12:04:37 +0800

    feat 新增代码注释

--- leaf-base-datasource/src/main/java/cn/maple/core/datasource/service/GXMybatisListenerService.java
+++ leaf-base-datasource/src/main/java/cn/maple/core/datasource/service/GXMybatisListenerService.java
@@ -7,18 +7,40 @@ import org.slf4j.LoggerFactory;
 public interface GXMybatisListenerService<T> {
     Logger LOG = LoggerFactory.getLogger(GXMybatisListenerService.class);
 
+    /**
+     * 保存实体的监听器
+     *
+     * @param data 实体数据
+     */
     default void saveEntityListener(T data) {
         LOG.info("请自定义实现saveEntityListener监听逻辑");
     }
 
+    /**
+     * 更新实体的监听器
+     *
+     * @param data             更新之后的实体数据
+     * @param keyValuePairs    本次更新的条件
+     * @param keyOperatorPairs 每个字段的操作
+     */
     default void updateEntityListener(T data, Dict keyValuePairs, Dict keyOperatorPairs) {
         LOG.info("请自定义实现updateEntityListener监听逻辑");
     }
 
+    /**
+     * 更新字段的监听器
+     *
+     * @param data 更新的字段信息
+     */
     default void updateFieldListener(Dict data) {
         LOG.info("请自定义实现updateFieldListener监听逻辑");
     }
 
+    /**
+     * 软删除监听器
+     *
+     * @param data 删除的条件
+     */
     default void deleteSoftListener(Dict data) {
         LOG.info("请自定义实现deleteSoftListener监听逻辑");
     }
