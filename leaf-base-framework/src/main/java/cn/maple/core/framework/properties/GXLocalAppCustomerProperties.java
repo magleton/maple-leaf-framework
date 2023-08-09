92c1bb2080919f22219abf4450ef13e7b8f02e8b
塵渊 <britton@126.com>
2023-08-09 16:54:33 +0800

    add 新增应用自定义的环境变量配置文件 目的是为了分类自定义的环境变量与系统组件定义的环境变量

---
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/properties/GXLocalAppCustomerProperties.java
@@ -0,0 +1,22 @@
+package cn.maple.core.framework.properties;
+
+import cn.maple.core.framework.factory.GXYamlPropertySourceFactory;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * 每个应用自定义的环境配置文件
+ */
+@Data
+@Slf4j
+@Component
+@SuppressWarnings("all")
+@ConditionalOnMissingClass({"com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties"})
+@PropertySource(value = {"classpath:/${spring.profiles.active}/app-customer.yml"}, factory = GXYamlPropertySourceFactory.class, ignoreResourceNotFound = false)
+@ConfigurationProperties(prefix = "")
+public class GXLocalAppCustomerProperties {
+}
