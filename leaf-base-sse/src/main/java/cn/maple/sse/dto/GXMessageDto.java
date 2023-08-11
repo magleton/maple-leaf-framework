e81b7c3e2f4817e4a60f820c9e151cf9ed428069
塵渊 <britton@126.com>
2023-08-11 13:02:51 +0800

    add 新增sse模块

---
+++ leaf-base-sse/src/main/java/cn/maple/sse/dto/GXMessageDto.java
@@ -0,0 +1,22 @@
+package cn.maple.sse.dto;
+
+import cn.maple.core.framework.dto.GXBaseDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class GXMessageDto extends GXBaseDto {
+    /**
+     * 客户端id
+     */
+    private String clientId;
+    /**
+     * 传输数据体(json)
+     */
+    private String data;
+}
---
+++ leaf-base-sse/src/main/java/cn/maple/sse/service/GXSseEmitterService.java
@@ -0,0 +1,45 @@
+package cn.maple.sse.service;
+
+import cn.maple.core.framework.service.GXBusinessService;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+/**
+ * 基于Spring Boot实现的Server-sent events
+ */
+public interface GXSseEmitterService extends GXBusinessService {
+    /**
+     * 创建连接
+     *
+     * @param clientId 客户端ID
+     */
+    SseEmitter createConnect(String clientId);
+
+    /**
+     * 根据客户端id获取SseEmitter对象
+     *
+     * @param clientId 客户端ID
+     */
+    SseEmitter getSseEmitterByClientId(String clientId);
+
+    /**
+     * 发送消息给所有客户端
+     *
+     * @param msg 消息内容
+     */
+    void sendMessageToAllClient(String msg);
+
+    /**
+     * 给指定客户端发送消息
+     *
+     * @param clientId 客户端ID
+     * @param msg      消息内容
+     */
+    void sendMessageToOneClient(String clientId, String msg);
+
+    /**
+     * 关闭连接
+     *
+     * @param clientId 客户端ID
+     */
+    void closeConnect(String clientId);
+}
