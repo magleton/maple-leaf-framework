5fe2d64fb587be6401e5cfcdd87f62b79334a1d5
塵渊 <britton@126.com>
2023-08-22 15:34:20 +0800

    add 新增seata模块

---
+++ leaf-base-seata/src/main/java/cn/maple/seata/properties/GXNacosSeataProperties.java
@@ -0,0 +1,29 @@
+package cn.maple.nacos.properties;
+
+import com.alibaba.nacos.api.annotation.NacosProperties;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 每个应用自定义的环境配置文件
+ */
+@Data
+@Slf4j
+@Component
+@SuppressWarnings("all")
+@ConditionalOnClass(name = {"com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties"})
+@NacosPropertySource(dataId = "seata.yml",
+        groupId = "${spring.cloud.nacos.config.group:${nacos.config.group:}}",
+        properties = @NacosProperties(
+                serverAddr = "${spring.cloud.nacos.config.server-addr:${nacos.config.server-addr:}}",
+                namespace = "${spring.cloud.nacos.config.namespace:${nacos.config.namespace:}}",
+                username = "${spring.cloud.nacos.username:${nacos.config.username:}}",
+                password = "${spring.cloud.nacos.password:${nacos.config.password:}}"),
+        autoRefreshed = true)
+@ConfigurationProperties(prefix = "")
+public class GXNacosSeataProperties {
+}
