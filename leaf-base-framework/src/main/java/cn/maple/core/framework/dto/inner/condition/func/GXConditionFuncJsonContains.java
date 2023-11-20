32373f90db3de2c058670c634f3406d43821884d
塵子曦 <britton@126.com>
2023-11-20 16:02:50 +0800

    feat 完善json_container方法的多值查询

--- leaf-base-framework/src/main/java/cn/maple/core/framework/dto/inner/condition/func/GXConditionFuncJsonContains.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/dto/inner/condition/func/GXConditionFuncJsonContains.java
@@ -6,16 +6,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class GXConditionFuncJsonContains extends GXConditionFunc<String> {
-    private final List<String> values;
+    private final List<Object> values;
 
-    private final String jsonPath;
+    private String jsonPath;
 
-    public GXConditionFuncJsonContains(String tableNameAlias, String op, List<String> values) {
-        this(tableNameAlias, op, values, "$");
+    public GXConditionFuncJsonContains(String tableNameAlias, String jsonField, List<Object> values) {
+        this(tableNameAlias, jsonField, values, "$");
     }
 
-    public GXConditionFuncJsonContains(String tableNameAlias, String op, List<String> values, String jsonPath) {
-        super(tableNameAlias, op, "", null);
+    public GXConditionFuncJsonContains(String tableNameAlias, String jsonField, List<Object> values, String jsonPath) {
+        super(tableNameAlias, jsonField, "", null);
         this.values = values;
         this.jsonPath = jsonPath;
     }
@@ -27,13 +27,24 @@ public class GXConditionFuncJsonContains extends GXConditionFunc<String> {
 
     @Override
     public String getFieldExpression() {
+        if (CharSequenceUtil.isEmpty(jsonPath)) {
+            jsonPath = "$";
+        } else {
+            jsonPath = CharSequenceUtil.format("$.{}", jsonPath);
+        }
         String format = "`{}`.`{}`->'" + jsonPath + "', CAST('[{}]' AS JSON)";
         return CharSequenceUtil.format(format, tableNameAlias);
     }
 
     @Override
     public String getFieldValue() {
-        return values.stream().map(s -> CharSequenceUtil.format("\"{}\"", s)).collect(Collectors.joining(","));
+        return values.stream().map(s -> {
+            String format = "\"{}\"";
+            if (s.getClass().isAssignableFrom(Integer.class) || s.getClass().isAssignableFrom(Long.class) || s.getClass().isAssignableFrom(Short.class)) {
+                format = "{}";
+            }
+            return CharSequenceUtil.format(format, s);
+        }).collect(Collectors.joining(","));
     }
 
     @Override
