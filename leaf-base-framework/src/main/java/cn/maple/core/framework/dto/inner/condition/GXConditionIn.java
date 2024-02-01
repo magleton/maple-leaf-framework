398efc83a5145924d9c4b6af250c637cdaccfefb
塵子曦 <britton@126.com>
2024-02-01 09:33:54 +0800

    feat 限制数据库的in条件不能超过300条

--- leaf-base-framework/src/main/java/cn/maple/core/framework/dto/inner/condition/GXConditionIn.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/dto/inner/condition/GXConditionIn.java
@@ -1,6 +1,8 @@
 package cn.maple.core.framework.dto.inner.condition;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.text.CharSequenceUtil;
+import cn.maple.core.framework.exception.GXBusinessException;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -17,6 +19,9 @@ public class GXConditionIn extends GXCondition<String> {
 
     @Override
     public String getFieldValue() {
+        if (CollUtil.size(value) > 300) {
+            throw new GXBusinessException("IN查询条件不能超过300条数据!");
+        }
         String str = ((Set<Number>) value).stream().map(String::valueOf).collect(Collectors.joining(","));
         return CharSequenceUtil.format("({})", str);
     }
