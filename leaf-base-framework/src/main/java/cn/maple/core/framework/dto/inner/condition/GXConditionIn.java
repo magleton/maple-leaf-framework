61391758dff6717d4df735ea0fd123ca3ba5e75d
塵子曦 <britton@126.com>
2024-03-27 09:54:50 +0800

    feat 修复in条件限制问题

--- leaf-base-framework/src/main/java/cn/maple/core/framework/dto/inner/condition/GXConditionIn.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/dto/inner/condition/GXConditionIn.java
@@ -7,7 +7,6 @@ import cn.maple.core.framework.exception.GXBusinessException;
 import cn.maple.core.framework.util.GXCommonUtils;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -27,7 +26,7 @@ public class GXConditionIn extends GXCondition<String> {
         int limitCnt = 100000;
         List<String> envLst = CollUtil.newArrayList(GXCommonConstant.RUN_ENV_DEV, GXCommonConstant.RUN_ENV_LOCAL);
         if (CollUtil.contains(envLst, activeProfile)/* && GXCurrentRequestContextUtils.isHTTP()*/) {
-            limitCnt = Optional.ofNullable(GXCommonUtils.getEnvironmentValue("db.in.limit.cnt", Integer.class)).orElse(50);
+            limitCnt = GXCommonUtils.getEnvironmentValue("db.in.limit.cnt", Integer.class, 50);
         }
         if (CollUtil.size(value) > limitCnt) {
             throw new GXBusinessException(CharSequenceUtil.format("IN查询条件不能超过{}条数据!", limitCnt));
