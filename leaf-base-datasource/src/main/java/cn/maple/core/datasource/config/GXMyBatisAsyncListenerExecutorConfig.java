ff48122e0abde5d47ea9660a5facea91cdf22c2f
塵子曦 <britton@126.com>
2024-05-29 10:42:50 +0800

    feat 异步任务使用虚拟线程

--- leaf-base-datasource/src/main/java/cn/maple/core/datasource/config/GXMyBatisAsyncListenerExecutorConfig.java
+++ leaf-base-datasource/src/main/java/cn/maple/core/datasource/config/GXMyBatisAsyncListenerExecutorConfig.java
@@ -5,14 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.core.task.support.TaskExecutorAdapter;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 @Configuration
 public class GXMyBatisAsyncListenerExecutorConfig {
     @Bean("myBatisEventAsyncTaskExecutor")
     public AsyncTaskExecutor myBatisEventAsyncTaskExecutor() {
-        ExecutorService virtualThreadPerTaskExecutor = Executors.newVirtualThreadPerTaskExecutor();
-        return new TaskExecutorAdapter(virtualThreadPerTaskExecutor);
+        Thread.Builder.OfVirtual ofVirtual = Thread.ofVirtual().name("virtual-mybatis-listener#", 1);
+        ThreadFactory factory = ofVirtual.factory();
+        return new TaskExecutorAdapter(Executors.newThreadPerTaskExecutor(factory));
     }
 }
---
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/config/AsyncTaskConfiguration.java
@@ -0,0 +1,24 @@
+package cn.maple.core.framework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * 异步任务
+ */
+@Configuration
+@EnableAsync
+public class AsyncTaskConfiguration implements AsyncConfigurer {
+    @Override
+    public Executor getAsyncExecutor() {
+        Thread.Builder.OfVirtual ofVirtual = Thread.ofVirtual().name("virtual-async#", 1);
+        ThreadFactory factory = ofVirtual.factory();
+        return new TaskExecutorAdapter(Executors.newThreadPerTaskExecutor(factory));
+    }
+}
