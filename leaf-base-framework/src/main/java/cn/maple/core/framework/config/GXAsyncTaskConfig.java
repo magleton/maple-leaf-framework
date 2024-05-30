d8bd027ced6c1b17508c06032e66e4acce383470
塵子曦 <britton@126.com>
2024-05-30 10:02:27 +0800

    feat 删除对虚拟线程的支持 由于目前jenkins只支持到17

--- leaf-base-framework/src/main/java/cn/maple/core/framework/config/GXAsyncTaskConfig.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/config/GXAsyncTaskConfig.java
@@ -1,24 +1,36 @@
 package cn.maple.core.framework.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.support.TaskExecutorAdapter;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * 异步任务
  */
 @Configuration
-@EnableAsync
 public class GXAsyncTaskConfig implements AsyncConfigurer {
     @Override
     public Executor getAsyncExecutor() {
-        Thread.Builder.OfVirtual ofVirtual = Thread.ofVirtual().name("virtual-async#", 1);
-        ThreadFactory factory = ofVirtual.factory();
-        return new TaskExecutorAdapter(Executors.newThreadPerTaskExecutor(factory));
+        ThreadPoolTaskExecutor poolTaskExecutor = new ThreadPoolTaskExecutor();
+        // 设置线程池关闭的时候需要等待所有任务都完成 才能销毁其他的Bean
+        poolTaskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+        // 设置核心池大小
+        poolTaskExecutor.setCorePoolSize(1);
+        // 设置最大池大小，只有在缓冲队列满了之后才会申请超过核心线程数的线程
+        poolTaskExecutor.setMaxPoolSize(2);
+        // 设置队列容量
+        poolTaskExecutor.setQueueCapacity(1000);
+        // 设置保持活动秒数，当超过了核心线程数之外的线程在空闲时间到达之后会被销毁
+        poolTaskExecutor.setKeepAliveSeconds(120);
+        // 设置线程名称前缀
+        poolTaskExecutor.setThreadNamePrefix("async-task-thread-pool-");
+        // 设置拒绝的执行处理策略
+        poolTaskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        // 初始化线程池
+        poolTaskExecutor.initialize();
+        return poolTaskExecutor;
     }
 }
