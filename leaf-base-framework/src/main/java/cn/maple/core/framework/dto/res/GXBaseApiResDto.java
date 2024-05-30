410a832beaf8f6cd9d5307da2f797fa881313e58
塵子曦 <britton@126.com>
2024-05-30 11:11:06 +0800

    feat 处理GXBaseApiResDto冲突问题

--- leaf-base-framework/src/main/java/cn/maple/core/framework/dto/res/GXBaseApiResDto.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/dto/res/GXBaseApiResDto.java
@@ -3,7 +3,11 @@ package cn.maple.core.framework.dto.res;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+/**
+ * @deprecated
+ */
 @Data
 @EqualsAndHashCode(callSuper = true)
+@Deprecated(forRemoval = true, since = "3.2.1")
 public class GXBaseApiResDto extends GXBaseResDto {
 }
