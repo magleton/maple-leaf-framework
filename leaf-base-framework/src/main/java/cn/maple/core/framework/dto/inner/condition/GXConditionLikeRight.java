88a28815e174bb7fb6338d1422df9a8d3b81306a
塵子曦 <britton@126.com>
2024-06-03 09:39:19 +0800

    feat 处理数据字段特殊字符

--- leaf-base-framework/src/main/java/cn/maple/core/framework/dto/inner/condition/GXConditionLikeRight.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/dto/inner/condition/GXConditionLikeRight.java
@@ -14,6 +14,9 @@ public class GXConditionLikeRight extends GXCondition<String> {
 
     @Override
     public String getFieldValue() {
+        if (CharSequenceUtil.startWith(value.toString(), '\'') && CharSequenceUtil.endWith(value.toString(), '\'')) {
+            return value.toString();
+        }
         return CharSequenceUtil.format("'{}%'", value);
     }
 }
