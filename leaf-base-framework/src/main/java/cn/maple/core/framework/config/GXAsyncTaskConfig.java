86caf634339747a116d4671b2147b03a41e165a0
塵子曦 <britton@126.com>
2024-06-04 16:53:06 +0800

    feat 完善异步任务使用虚拟线程的配置

--- leaf-base-framework/src/main/java/cn/maple/core/framework/config/GXAsyncTaskConfig.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/config/GXAsyncTaskConfig.java
@@ -3,7 +3,6 @@ package cn.maple.core.framework.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.support.TaskExecutorAdapter;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -13,7 +12,6 @@ import java.util.concurrent.ThreadFactory;
  * 异步任务
  */
 @Configuration
-@EnableAsync
 public class GXAsyncTaskConfig implements AsyncConfigurer {
     @Override
     public Executor getAsyncExecutor() {
