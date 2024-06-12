891fde3cd5d27c7d850ab9ef0e0472c833fb58fa
塵子曦 <britton@126.com>
2024-06-12 18:23:26 +0800

    feat 还原Dubbo的API

---
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/dto/res/GXBaseApiResDto.java
@@ -0,0 +1,10 @@
+package cn.maple.core.framework.dto.res;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Deprecated(since = "4.0.3" , forRemoval = true)
+public class GXBaseApiResDto extends GXBaseResDto {
+}
