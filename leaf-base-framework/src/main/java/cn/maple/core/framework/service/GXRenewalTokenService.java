cb2529e38ef790059bf39dc82f894e6a991b084b
塵子曦 <britton@126.com>
2024-06-19 10:22:40 +0800

    feat 错别字修改 刷新token新增自定义参数

--- leaf-base-framework/src/main/java/cn/maple/core/framework/service/GXRenewalTokenService.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/service/GXRenewalTokenService.java
@@ -1,5 +1,6 @@
 package cn.maple.core.framework.service;
 
+import cn.hutool.core.lang.Dict;
 import cn.maple.core.framework.exception.GXBusinessException;
 
 /**
@@ -21,9 +22,10 @@ public interface GXRenewalTokenService {
     /**
      * 重新获取Token
      *
+     * @param param 自定义参数
      * @return 新的token
      */
-    default String refreshToken() {
+    default String refreshToken(Dict param) {
         throw new GXBusinessException("请自定义实现Token无感刷新的逻辑");
     }
 }
