806fb8e7830433dd856cbd2dd1036a7a1850c7cc
塵子曦 <britton@126.com>
2024-07-12 18:03:24 +0800

    feat: 完善debezium模块的Debezium引擎初始化时在项目是集群部署下只需要一个服务读取二进制即可

--- leaf-base-debezium/src/main/java/cn/maple/debezium/framework/GXDebeziumCommandLineRunnerServiceImpl.java
+++ leaf-base-debezium/src/main/java/cn/maple/debezium/framework/GXDebeziumCommandLineRunnerServiceImpl.java
@@ -5,6 +5,7 @@ import cn.hutool.core.text.CharSequenceUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.json.JSONUtil;
 import cn.maple.core.framework.service.GXCommandLineRunnerService;
+import cn.maple.core.framework.util.GXCommonUtils;
 import cn.maple.core.framework.util.GXSpringContextUtils;
 import cn.maple.debezium.exception.GXDebeziumInitialException;
 import cn.maple.debezium.properties.GXDebeziumProperties;
@@ -14,19 +15,23 @@ import io.debezium.engine.DebeziumEngine;
 import io.debezium.engine.format.Json;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.task.TaskExecutor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
-@Component
+@Service
 @Order
 @Slf4j
-public class GXDebeziumCommandLineRunnerServiceImpl implements GXCommandLineRunnerService {
+public class GXDebeziumCommandLineRunnerServiceImpl implements GXCommandLineRunnerService, DisposableBean {
+    private final Map<String, DebeziumEngine<ChangeEvent<String, String>>> debeziumEngineMap = new ConcurrentHashMap<>();
+
     @Resource
     private GXDebeziumProperties debeziumProperties;
 
@@ -41,6 +46,7 @@ public class GXDebeziumCommandLineRunnerServiceImpl implements GXCommandLineRunn
             return;
         }
         debeziumProperties.getConfig().forEach((key, map) -> {
+            debeziumService.initialEngineLock(key);
             Properties properties = new Properties();
             Set<Map.Entry<String, String>> entries = map.entrySet();
             for (Map.Entry<String, String> entry : entries) {
@@ -55,10 +61,27 @@ public class GXDebeziumCommandLineRunnerServiceImpl implements GXCommandLineRunn
                         debeziumService.processCaptureDataChange(dbChangeData);
                     }).build()
             ) {
+                debeziumEngineMap.put(key, engine);
                 debeziumExecutor.execute(engine);
             } catch (IOException e) {
                 throw new GXDebeziumInitialException(CharSequenceUtil.format("初始化Debezium内嵌引擎{}失败", key), e);
+            } finally {
+                debeziumService.initialEngineUnLock(key);
             }
         });
+    }
+
+    @Override
+    public void destroy() {
+        for (Map.Entry<String, DebeziumEngine<ChangeEvent<String, String>>> entry : debeziumEngineMap.entrySet()) {
+            try {
+                DebeziumEngine<ChangeEvent<String, String>> engine = entry.getValue();
+                engine.close();
+            } catch (IOException e) {
+                log.error("关闭{}>>>Debezium引擎失败", entry.getKey(), e);
+            }
+        }
+        String appName = GXCommonUtils.getEnvironmentValue("spring.application.name", String.class);
+        log.info("~~~~ 应用{}退出了 , 再见 ~~~~~", appName);
     }
 }
