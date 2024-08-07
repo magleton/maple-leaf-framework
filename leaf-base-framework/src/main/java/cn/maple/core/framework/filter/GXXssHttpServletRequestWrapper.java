c3849b30ecf4d8099ca2eec5445a78de677d6c59
塵子曦 <britton@126.com>
2024-08-07 15:48:54 +0800

    feat: 处理请求流只能使用一次的问题

--- leaf-base-framework/src/main/java/cn/maple/core/framework/filter/GXXssHttpServletRequestWrapper.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/filter/GXXssHttpServletRequestWrapper.java
@@ -2,15 +2,19 @@ package cn.maple.core.framework.filter;
 
 import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.text.CharSequenceUtil;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-
+import com.google.common.io.ByteStreams;
 import jakarta.servlet.ReadListener;
 import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletRequestWrapper;
+import lombok.SneakyThrows;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -27,11 +31,18 @@ public class GXXssHttpServletRequestWrapper extends HttpServletRequestWrapper {
     /**
      * 没被包装过的HttpServletRequest(特殊场景, 需要自己过滤)
      */
-    HttpServletRequest orgRequest;
+    private final HttpServletRequest orgRequest;
 
+    /**
+     * 用于将流保存下来
+     */
+    private final byte[] requestBody;
+
+    @SneakyThrows
     public GXXssHttpServletRequestWrapper(HttpServletRequest request) {
         super(request);
         orgRequest = request;
+        requestBody = ByteStreams.toByteArray(request.getInputStream());
     }
 
     /**
@@ -45,6 +56,11 @@ public class GXXssHttpServletRequestWrapper extends HttpServletRequestWrapper {
     }
 
     @Override
+    public BufferedReader getReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(new ByteArrayInputStream(requestBody)));
+    }
+
+    @Override
     public ServletInputStream getInputStream() throws IOException {
         // 非json类型，直接返回
         if (!MediaType.APPLICATION_JSON_VALUE.equalsIgnoreCase(super.getHeader(HttpHeaders.CONTENT_TYPE))) {
@@ -52,7 +68,8 @@ public class GXXssHttpServletRequestWrapper extends HttpServletRequestWrapper {
         }
 
         // 为空，直接返回
-        String json = IoUtil.read(super.getInputStream(), StandardCharsets.UTF_8);
+        //String json = IoUtil.read(super.getInputStream(), StandardCharsets.UTF_8);
+        String json = IoUtil.read(new ByteArrayInputStream(requestBody), StandardCharsets.UTF_8);
         if (CharSequenceUtil.isBlank(json)) {
             return super.getInputStream();
         }
@@ -63,7 +80,7 @@ public class GXXssHttpServletRequestWrapper extends HttpServletRequestWrapper {
         return new ServletInputStream() {
             @Override
             public boolean isFinished() {
-                return true;
+                return bis.available() == 0;
             }
 
             @Override
